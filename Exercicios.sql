7254f467a24db633a49e899431139aa89368dff0
Edeilson Gonçalves <edeilsoniasd@outlook.com>
2024-06-25 10:24:51 -0300

    Mais exercícios

--- Exercicios.sql
+++ Exercicios.sql
@@ -310,30 +310,138 @@ from sales.products
 group by brand
 having count(*) > 10 
 
+----------------------------------------------------------------------------------------------
+-- JOIN
+
+-- Exemplo 1 - Utilize o LEFT JOIN para fazer join entre as tabelas
+-- 	temp_tables.tabela_1 e temp_tables.tabela_2
+select * from temp_tables.tabela_1
+select * from temp_tables.tabela_2
+
+select t1.cpf, t1.name, t2.state
+from temp_tables.tabela_1 as t1
+left join temp_tables.tabela_2 as t2
+on t1.cpf = t2.cpf
+
+-- Exemplo 2 - Utilize o INNER JOIN para fazer join entre as tabelas
+-- 	temp_tables.tabela_1 e temp_tables.tabela_2	
+
+select t1.cpf, t1.name, t2.state
+from temp_tables.tabela_1 as t1
+inner join temp_tables.tabela_2 as t2
+on t1.cpf = t2.cpf
+
+-- Exemplo 3 - Utilize o RIGHT JOIN para fazer join entre as tabelas
+-- 	temp_tables.tabela_1 e temp_tables.tabela_2	
+
+select t2.cpf, t1.name, t2.state
+from temp_tables.tabela_1 as t1
+right join temp_tables.tabela_2 as t2
+on t1.cpf = t2.cpf
+
+-- Exemplo 4 - Utilize o RIGHT JOIN para fazer join entre as tabelas
+-- 	temp_tables.tabela_1 e temp_tables.tabela_2	
+
+select t2.cpf, t1.name, t2.state
+from temp_tables.tabela_1 as t1
+full join temp_tables.tabela_2 as t2
+on t1.cpf = t2.cpf
 
+-- Exercícios
 
+-- Exemplo 1 - Identifique qual é o status profissional mais frequente nos clientes 
+-- que compraram automóveis no site
+select 
+	cus.professional_status,
+	count(fun.paid_date) as pagamentos
+	
+from sales.funnel as fun
+left join sales.customers as cus
+	on fun.customer_id = cus.customer_id
+group by cus.professional_status
+order by pagamentos desc
 
+-- Exemplo 2 - Identifique qual é o gênero mais frequente nos clientes que compraram 
+-- automóveis no site. Obs: utilizar a tabela temp_tables.ibge_genders
 
+select * from temp_tables.ibge_genders limit 10
 
 
+select 
+	ibge.gender,
+	count(fun.paid_date)
+from sales.funnel as fun
+left join sales.customers as cus
+	on fun.customer_id = cus.customer_id
+left join temp_tables.ibge_genders as ibge
+	on lower(cus.first_name) = ibge.first_name
+group by ibge.gender
 
+-- (Exemplo 3) Identifique de quais regiões são os clientes que mais visitam o site
+-- Obs: Utilizar a tabela temp_tables.regions
 
+select * from sales.customers limit 10
+select * from temp_tables.regions limit 10
 
+select
+	reg.region,
+	count(fun.visit_page_date) as visitas
+from sales.funnel as fun
+left join sales.customers as cus
+	on fun.customer_id = cus.customer_id
+left join temp_tables.regions as reg
+	on lower(cus.city) = lower(reg.city)
+	and lower(cus.state) = lower(reg.state)
+group by reg.region
+order by visitas desc
 
+-- EXERCÍCIOS ########################################################################
 
+-- (Exercício 1) Identifique quais as marcas de veículo mais visitada na tabela sales.funnel
 
+select 
+	pro.brand,
+	count(*) as visitas
 
+from sales.funnel as fun
+left join sales.products as pro
+	on fun.product_id = pro.product_id
+group by pro.brand
+order by visitas desc
 
 
+-- (Exercício 2) Identifique quais as lojas de veículo mais visitadas na tabela sales.funnel
 
+select 
+	sto.store_name,
+	count(*) as visitas
 
+from sales.funnel as fun
+left join sales.stores as sto
+	on fun.store_id = sto.store_id
+group by sto.store_name
+order by visitas desc
 
 
 
+-- (Exercício 3) Identifique quantos clientes moram em cada tamanho de cidade (o porte da cidade
+-- consta na coluna "size" da tabela temp_tables.regions)
 
+select
+	reg.size,
+	count(*) as contagem
+from sales.customers as cus
+left join temp_tables.regions as reg
+	on lower(cus.city) = lower(reg.city)
+	and lower(cus.state) = lower(reg.state)
+group by reg.size
+order by contagem
 
+-----------------------------------------------------------------------------------------
 
+-- UNIONS
 
+-- 
 
 
 
@@ -375,7 +483,14 @@ having count(*) > 10
 
 
 
---
+
+
+
+
+
+
+
+
 
 
 
